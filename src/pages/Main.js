ab235112e08893d2ca4f09272a399525264d8013
André Machado <andrels.machado82@gmail.com>
2019-10-11 18:29:05 -0300

    Buscando os usuários da api

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,31 +1,50 @@
-import React from 'react';
+import React, {  useState, useEffect } from 'react';
 import './Main.css';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
 
 export default function Main({ match }) {
+  const [users, setUsers] = useState([]); 
+
+  useEffect(() => {
+    async function loadUsers(){
+      const response = await  api.get('/devs', {
+        headers: {
+          user: match.params.id,
+        }
+      })
+      
+      setUsers(response.data);
+    } 
+    loadUsers();
+  }, [match.params.id])
+
   return (
     <div className="main-container">
       <img src={logo} alt="Tindev" />
       <ul>
-        <li>
-          <img src="https://avatars2.githubusercontent.com/u/861751?v=4" alt=""/>
-          <footer>
-            <strong>Robson</strong>
-            <p>Co-founder e CEO @Rocketseat</p>
-          </footer>
+        { users.map(user => (
+          <li key={user._id}>          
+            <img src={user.avatar} alt={user.name}/>
+            <footer>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </footer>
 
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike"/>
-            </button>
-            <button type="button">
-              <img src={like} alt="like"/>              
-            </button>
-          </div>
-        </li>
+            <div className="buttons">
+              <button type="button">
+                <img src={dislike} alt="dislike"/>
+              </button>
+              <button type="button">
+                <img src={like} alt="like"/>              
+              </button>
+            </div>
+          </li>
+        ))}         
       </ul>
     </div>
   );
