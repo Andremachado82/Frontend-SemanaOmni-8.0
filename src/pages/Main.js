f53360d9d5e80eb4ce8046d7986852860d740c9a
André Machado <andrels.machado82@gmail.com>
2019-10-11 18:07:04 -0300

    Estilizando página Main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import './Main.css';
 
-export default function Main() {
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
+export default function Main({ match }) {
   return (
-    <h1>Wellow</h1>
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+      <ul>
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/861751?v=4" alt=""/>
+          <footer>
+            <strong>Robson</strong>
+            <p>Co-founder e CEO @Rocketseat</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="like"/>              
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 }
